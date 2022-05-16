e94c22c969e5d211ce53288d43b306d8acc76488
Frederik Lüneborg Holm Jeppesen <58039193+FrederikJeppesen@users.noreply.github.com>
2022-05-16 10:01:10 +0200

    Add files via upload

--- Lineære regressioner/linear_regression.py
+++ Lineære regressioner/linear_regression.py
@@ -7,41 +7,46 @@ path = "/Users/frederikluneborgholmjeppesen/Documents/Universitetet/3. år/Bach
 
 # NOTICE: remember to choose the correct dataset
 dataset = pd.read_excel(path + 'combined_data.xlsx')
-dataset['Fuel'].replace(['El', 'Benzin', 'Diesel'], [0, 1, 2], inplace = True)
+
+# Changing the measure of 'Prices (2015-DKK)' into 100000 kr.
+dataset['Prices (2015-DKK)'] = dataset['Prices (2015-DKK)'] / 100000
+
+# Changing the measure of 'Weight (kg)' into tons
+dataset['Weight (kg)'] = dataset['Weight (kg)'] / 1000
+
+# Changing the measure of 'Engine effect' into 100 kW
+dataset['Engine effect (kW)'] = dataset['Engine effect (kW)'] / 100
+
 print("Dataset:\n", dataset)
 
-# MAKING LINEAR REGRESSION
-dataset = dataset.values
-
-y = dataset[:, 1].astype(float)
-y = pd.DataFrame({'Shares' : y})
+y = dataset['Markedsandele'].values.astype(float)
+y = pd.DataFrame({'Markedsandele' : y})
 print("\ny:\n", y)
 
-x1 = dataset[:, 2].astype(str)
+x1 = dataset['Weight (kg)'].values.astype(float)
 x2 = np.ones(x1.shape).astype(float)
-x3 = dataset[:, 3].astype(float)
-x4 = dataset[:, 4].astype(float)
-x5 = dataset[:, 7].astype(float)
-x6 = dataset[:, 5].astype(float)
+x3 = dataset['Engine effect (kW)'].values.astype(float)
+x4 = dataset['Size (m3)'].values.astype(float)
+x5 = dataset['Prices (2015-DKK)'].values.astype(float)
+x6 = dataset['Cost/km (DKK)'].values.astype(float)
+x7 = dataset['kmL'].values.astype(float)
+x8 = dataset['Fuel'].values.astype(str)
 
-x = pd.DataFrame({'Constant' : x2, 'Fuel' : x1, 'Weight' : x3, 'Engine effect' : x4, 'Fuel efficiency (km/l)' : x5, 'Price' : x6})
+x = pd.DataFrame({'Konstant' : x2, 'Vægt (tons)' : x1, 'Bilmotorens effekt (100 kW)' : x3, 'Størrelse (m3)' : x4, 'Priser (100.000 DKK)' : x5, 'Omkostninger/km (DKK)' : x6, 'Drivkraftens effektivitet (km/l)' : x7, 'Drivkraft': x8})
 x = pd.get_dummies(data = x, drop_first = True)
 print("\nX:\n", x)
 
 y_label = list(y.columns)[0]
 print("\ny_label:", y_label)
-
 x_labels = list(x.columns)
 print("x_labels:", x_labels)
 
 y = np.array(y)
-#print("y:\n", y)
-
+print("\ny:\n", y)
 x = np.array(x)
-#print("X:\n", x)
+print("\nX:\n", x)
 
 pols = LM.estimate(y, x, robust_se = True)
 
 print("\n")
-LM.print_table((y_label, x_labels), pols, title = "Linear regression", floatfmt = '.15f')
-
+table = LM.print_table((y_label, x_labels), pols, title = "Lineær regression")
